07cf017c1f8c925fe73c506b4ea25fc4495b108a
Bendo243 <nwosub99@gmail.com>
2024-08-13 11:02:11 +0100

    commit

---
+++ react-jobs/src/App.tsx
@@ -0,0 +1,21 @@
+import Nav from "./Components/Nav"
+import Hero from "./Components/Hero"
+import Homecards from "./Components/Homecards"
+import JobListing from "./Components/JobListing"
+import ViewAllJobs from "./Components/ViewAllJobs"
+
+
+
+const App = () => {
+  return (
+    <>
+    <Nav />
+    <Hero />
+    <Homecards />
+    <JobListing/>
+    <ViewAllJobs/>
+    </>
+  )
+}
+
+export default App
